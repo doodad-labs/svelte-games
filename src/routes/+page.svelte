71cbe7fd002f09a06c084987a19dbc2804c928e7
Reece Harris <reece.harris98@protonmail.com>
2025-03-25 10:17:29 +0000

    Add Snake game component and update README with game details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,9 @@
     import '../app.css';
     import Snap from '$lib/Snap.svelte';
     import TicTacToe from '$lib/TicTacToe.svelte';
+    import Snake from '$lib/Snake.svelte';
 
-    let selector: string = $state('snap');
+    let selector: string = $state('snake');
 
 </script>
 
@@ -13,6 +14,7 @@
         <select bind:value={selector} class="px-3 py-2 mt-0.5 w-full rounded border border-gray-200 shadow-sm sm:text-sm">
             <option value="snap">Snap</option>
             <option value="tictactoe">Tic Tac Toe</option>
+            <option value="snake">Snake</option>
         </select>
         
         <div>
@@ -20,6 +22,8 @@
                 <Snap />
             {:else if selector === 'tictactoe'}
                 <TicTacToe />
+            {:else if selector === 'snake'}
+                <Snake />
             {/if}
         </div>
         
